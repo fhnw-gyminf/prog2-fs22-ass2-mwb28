7525f0a8baa6888f3840a17be9c9162fecf3f99e
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-02-24 16:33:10 +0100

    implemented getCircumreference but not shure if this is correct...

--- src/main/java/ch/gyminf/u02/figures/Polygon.java
+++ src/main/java/ch/gyminf/u02/figures/Polygon.java
@@ -1,19 +1,34 @@
 package ch.gyminf.u02.figures;
 
 public class Polygon {
-	private Point[] xCoord;
-	private Point []yCoords;
-	
+	private int [] xCoords;
+	private int [] yCoords;
+
+
 
 
 
 	public Polygon(int[] xCoords, int[] yCoords) {
-		// TODO Auto-generated constructor stub
+		this.xCoords = xCoords;
+		this.yCoords = yCoords;
+
 	}
 
 	public double getCircumference() {
-		// TODO Auto-generated method stub
-		return 0;
+		double circumference =0;
+		double dx[] = new double [xCoords.length];
+		double dy[] = new double[yCoords.length];
+
+		for(int i = 0; i < xCoords.length; i++){
+			dx[i]= Math.abs(xCoords[i%xCoords.length] - xCoords[(i+1)%xCoords.length]);
+			dy[i]= Math.abs(yCoords[i%yCoords.length] - yCoords[(i+1)%yCoords.length]);
+			circumference += Math.sqrt(Math.pow(dx[i], 2) + Math.pow(dy[i], 2));
+
+
+
+		}
+
+		return circumference;
 	}
 
 	public double getArea() {
